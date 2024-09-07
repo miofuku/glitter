e9a09cc963ad6719078e46834b6e54894e0fd351
Bijun Li <bijunli86@gmail.com>
2024-09-07 22:49:41 +0200

    move to src

---
+++ src/blockchain.py
@@ -0,0 +1,60 @@
+import hashlib
+import time
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block_string = f"{self.index}{self.timestamp}{self.data}{self.previous_hash}"
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
+class PersonalBlockchain:
+    def __init__(self, owner):
+        self.chain = [self.create_genesis_block()]
+        self.owner = owner
+        self.private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048
+        )
+        self.public_key = self.private_key.public_key()
+
+    def create_genesis_block(self):
+        return Block(0, time.time(), "Genesis Block", "0")
+
+    def add_block(self, data):
+        previous_block = self.chain[-1]
+        new_block = Block(len(self.chain), time.time(), data, previous_block.hash)
+        self.chain.append(new_block)
+
+    def sign_data(self, data):
+        signature = self.private_key.sign(
+            data.encode(),
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        return signature
+
+    def verify_signature(self, data, signature):
+        try:
+            self.public_key.verify(
+                signature,
+                data.encode(),
+                padding.PSS(
+                    mgf=padding.MGF1(hashes.SHA256()),
+                    salt_length=padding.PSS.MAX_LENGTH
+                ),
+                hashes.SHA256()
+            )
+            return True
+        except:
+            return False
